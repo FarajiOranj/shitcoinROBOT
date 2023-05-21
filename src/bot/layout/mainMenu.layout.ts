b31e1277339d313654b6cb6c5172a538865311a6
Mohammadreza Faraji <farajioranj@gmail.com>
2023-05-21 17:30:08 +0330

    will be changed...

---
+++ src/bot/layout/mainMenu.layout.ts
@@ -0,0 +1,14 @@
+const reply_markup = {
+    keyboard: [
+        [
+            {text: 'نشانه گذاری', callback_data: 'track'}
+        ]
+    ]
+}
+
+
+/* 
+    Current Main Menu Reply Keyboard
+
+    [         نشانه گذاری          ]
+*/
