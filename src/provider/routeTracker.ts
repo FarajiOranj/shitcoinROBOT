40539b0eacc0aa99e331d88bdc5e64939ff40ac8
Mohammadreza Faraji <farajioranj@gmail.com>
2023-05-21 17:54:54 +0330

    tracker (single route) module done.

--- src/provider/routeTracker.ts
+++ src/provider/routeTracker.ts
@@ -56,7 +56,7 @@ export async function pendingTxTracker(
           ||
           tx.to !== eventName.toAddress
         )
-      ) {} 
+      ) {}
       else {
         console.log('Turning off the alchemy...');
         await alchemy.ws.off(eventName);
