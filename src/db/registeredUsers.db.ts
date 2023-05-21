561cbb02fbf3f74a28f727d6a0257756ad1fcd03
Mohammadreza Faraji <farajioranj@gmail.com>
2023-05-21 17:26:54 +0330

    handled users

---
+++ src/db/registeredUsers.db.ts
@@ -0,0 +1,13 @@
+let registeredUserIDS: Map<number, string | Array<string>> = new Map();
+
+registeredUserIDS.set(644473251, "0x0");
+registeredUserIDS.set(89192167, "0x0");
+registeredUserIDS.set(5443857391, "0x0");
+
+
+// type AddrRegExp = RegExp;
+// const evmAddrRegex: RegExp = /^(0x)?[0-9a-fA-F]{40}$/i;
+
+// function validateUserAddress(address: string): boolean {
+//     return evmAddrRegex.test(address);
+// } 
