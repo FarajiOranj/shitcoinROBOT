5eeccbcefb912cfc925ca663f52afc948677d9dc
Mohammadreza Faraji <farajioranj@gmail.com>
2023-05-21 17:29:58 +0330

    working on it...

---
+++ src/bot/handlers/handlers.ts
@@ -0,0 +1,10 @@
+import ITxData, {
+    IPendingTrackerFn,
+  } from '../../../public/types/transaction';
+
+
+import pendingTxTracker from '../../provider/routeTracker';
+
+const track = () => {
+
+}
