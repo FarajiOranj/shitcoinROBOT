accd85e0ec5c9d6ac394a6bf671aa4a491ad3cac
Mohammadreza Faraji <farajioranj@gmail.com>
2023-05-21 17:28:56 +0330

    working on it...

---
+++ app.ts
@@ -0,0 +1,24 @@
+import bot from './src/bot/bot.instance.ts';
+
+
+
+// fastify.get('/ws', { websocket: true }, (connection /*, request */) => {
+//   connection.socket.on('message', async (message) => {
+//     const txHash = message;
+//     const response = await axios.post(`https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`, {
+//       jsonrpc: '2.0',
+//       method: 'eth_getTransactionByHash',
+//       params: [txHash],
+//       id: 1,
+//     });
+
+//     const transaction = response.data.result;
+//     if (transaction && transaction.to.toLowerCase() === UNISWAP_ROUTER_ADDRESS.toLowerCase()) {
+//       bot.telegram.sendMessage(TELEGRAM_CHAT_ID, `Uniswap swap detected: ${txHash}`);
+//     }
+//   });
+// });
+
+
+
+bot.launch();
