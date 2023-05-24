44156cee9bce1f9843867964311fa69310e83f8e
farajioranj <farajioranj@gmail.com>
2023-05-25 01:55:17 +0430

    command moved to app

--- src/bot/commands/common.commands.ts
+++
@@ -1,9 +0,0 @@
-import { MiddlewareFn } from "telegraf";
-import bot from "../bot.instance";
-import { menuCB } from "../handlers/common.handlers";
-
-bot.action("start", menuCB);
-bot.start(menuCB);
-// bot.help();
-
-// export { starter, backward };
--- src/bot/commands/track.commands.ts
+++
@@ -1,4 +0,0 @@
-import ITxData, { IPendingTrackerFn } from "../../../public/types/transaction";
-import { MiddlewareFn } from "telegraf";
-
-// bot.action('track',); // bot.action('',);
