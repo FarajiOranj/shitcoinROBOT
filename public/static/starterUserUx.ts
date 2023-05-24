763bdb75e846e7cb9b47db11f4a4b5a1993ddc56
farajioranj <farajioranj@gmail.com>
2023-05-24 18:36:08 +0430

    messages for start and backward

---
+++ public/static/starterUserUx.ts
@@ -0,0 +1,10 @@
+const starterMessage = (name: string) :string => {
+     return `سلام ${name}\n
+     به ربات بلاکچینی خودت خیلی خوش اومدی\n
+     لطفا جهت راهنمایی گزینه مربوطه را انتخاب کنید`; 
+}
+
+const menuMessage: string = ':لطفا گزینه مد نظر خود را انتخاب کنید';
+
+
+export { starterMessage, menuMessage }
