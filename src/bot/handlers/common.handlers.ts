df47bb8f0c960bc440f155912c7cc5af08e6fc92
farajioranj <farajioranj@gmail.com>
2023-05-24 18:37:38 +0430

    common handleres almost done, start & back in 1 CB

---
+++ src/bot/handlers/common.handlers.ts
@@ -0,0 +1,16 @@
+import { Context, MiddlewareFn } from "telegraf";
+import bot from "../bot.instance";
+import { mainMenu } from "../layout/layout";
+import {
+  starterMessage,
+  menuMessage,
+} from "../../../public/static/starterUserUx";
+
+const menuCB = (ctx: Context) => {
+  const message: string =
+    (ctx.update as any)?.message?.text === "/start"
+      ? starterMessage(ctx.from.first_name ?? ctx.from.last_name)
+      : menuMessage;
+  bot.telegram.sendMessage(ctx.chat.id, message, mainMenu);
+};
+export { menuCB };
