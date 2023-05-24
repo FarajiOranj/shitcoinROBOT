3a162cf127d3a68fd5916027f31c474badd37233
farajioranj <farajioranj@gmail.com>
2023-05-24 23:44:22 +0430

    for test removed

--- dist/public/static/trackedAddresses.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const whaleAddresses = ["0xAf2358e98683265cBd3a48509123d390dDf54534"];
-exports.default = whaleAddresses;
