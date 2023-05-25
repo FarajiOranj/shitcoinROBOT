34ae9819d35cb2fac42ff3ba7f6eae2f97cca435
farajioranj <farajioranj@gmail.com>
2023-05-25 15:34:12 +0430

    saeed added as user

--- src/db/registeredUsers.db.ts
+++ src/db/registeredUsers.db.ts
@@ -3,7 +3,8 @@ let registeredUserIDS: Map<number, string | Array<string>> = new Map();
 registeredUserIDS.set(644473251, "0x0");
 registeredUserIDS.set(89192167, "0x0");
 registeredUserIDS.set(5443857391, "0x0");
-
+registeredUserIDS.set(390415311, "0x0");
+// registeredUserIDS.set(, "0x0");
 
 // type AddrRegExp = RegExp;
 // const evmAddrRegex: RegExp = /^(0x)?[0-9a-fA-F]{40}$/i;
