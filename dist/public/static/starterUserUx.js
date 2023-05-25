a32dd2818a4daca2889e38836fb05efaf776e8c7
farajioranj <farajioranj@gmail.com>
2023-05-25 06:43:15 +0430

    compiled

--- dist/public/static/starterUserUx.js
+++ dist/public/static/starterUserUx.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.menuMessage = exports.starterMessage = void 0;
 const starterMessage = (name) => {
-    return `سلام ${name} جان 👋 \nبه ربات بلاکچینی خودت خیلی خوش اومدی 🎉\nمیتونی برای راهنمایی بیشتر، از گزینه مربوطه استفاده کنی ✅`;
+    return `سلام ${name} جان 👋 \n\nبه ربات بلاکچینی خودت خیلی خوش اومدی 🎉\n\nمیتونی برای راهنمایی بیشتر، از گزینه مربوطه استفاده کنی ✅`;
 };
 exports.starterMessage = starterMessage;
 const menuMessage = ':لطفا گزینه مد نظر خود را انتخاب کنید';
