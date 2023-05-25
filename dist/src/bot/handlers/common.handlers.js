0e8b5c8c5249b482f0ec884b68820e3bb05e0ead
farajioranj <farajioranj@gmail.com>
2023-05-25 06:15:10 +0430

    compiled

--- dist/src/bot/handlers/common.handlers.js
+++ dist/src/bot/handlers/common.handlers.js
@@ -8,9 +8,9 @@ const bot_instance_1 = __importDefault(require("../bot.instance"));
 const layout_1 = require("../layout/layout");
 const starterUserUx_1 = require("../../../public/static/starterUserUx");
 const menuCB = (ctx) => {
-    var _a, _b, _c;
+    var _a, _b;
     const message = ((_b = (_a = ctx.update) === null || _a === void 0 ? void 0 : _a.message) === null || _b === void 0 ? void 0 : _b.text) === "/start"
-        ? (0, starterUserUx_1.starterMessage)((_c = ctx.from.first_name) !== null && _c !== void 0 ? _c : ctx.from.last_name)
+        ? (0, starterUserUx_1.starterMessage)(ctx.from.first_name)
         : starterUserUx_1.menuMessage;
     bot_instance_1.default.telegram.sendMessage(ctx.chat.id, message, layout_1.mainMenu);
 };
