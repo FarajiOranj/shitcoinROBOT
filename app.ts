6128d8db8ddee55a8dbf38769b3469cc6d901f59
farajioranj <farajioranj@gmail.com>
2023-05-28 07:29:36 +0430

    session added

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import bot from './src/bot/bot.instance';
+import './src/bot/session/default.session'
 import './src/bot/middlewares/common.middlewares';
 import './src/bot/commands/common.commands';
 import './src/bot/commands/track.commands';
