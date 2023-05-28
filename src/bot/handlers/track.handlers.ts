3f955b58965b5e0873dc5d26be4a64a54abd73f3
farajioranj <farajioranj@gmail.com>
2023-05-29 03:10:34 +0430

    working on address receiver listener + some cmnts

--- src/bot/handlers/track.handlers.ts
+++ src/bot/handlers/track.handlers.ts
@@ -2,29 +2,33 @@ import { Context } from "telegraf";
 // import { callbackQuery } from "telegraf/filters";
 import { SessionContext } from "telegraf/typings/session";
 import bot from "../bot.instance";
-import { backToMenu, trackMenu } from "../layout/layout";
-import trackSession from "../../../public/types/trackSession";
-import { trackOpts, fromAddres, toAddress } from "../../../public/static/trackUx";
+import { backToMenu, mainMenu, trackMenu } from "../layout/layout";
+import { TrackSession } from "../../../public/types/sessionTypes";
+import { trackOpts, fromAddres, toAddress, invalidAddress } from "../../../public/static/trackUx";
+import Web3 from "web3";
+import { menuMessage } from "../../../public/static/starterUserUx";
 
 const trackCB = (ctx: Context) => {
   ctx.deleteMessage();
   bot.telegram.sendMessage(ctx.chat.id, trackOpts, trackMenu);
 };
 
-const pairOptSaver = (ctx: SessionContext<trackSession>) => {
+const pairOptSaver = (ctx: SessionContext<TrackSession>) => {
   ctx.deleteMessage();
   const chatId: number = ctx.chat.id;
   const data: string = ctx.callbackQuery?.["data"];
 
-  ctx.session = {} as trackSession;
+  //! maybe should change or move
+  ctx.session = {} as TrackSession;
   
   ctx.session.userId = chatId;
   ctx.session.triggerType = data;
+  ctx.session.commonStat = "trackNotifier";
 
   const message: string = data === "toPaired" ? toAddress : fromAddres;
 
- /* 
-  needs more researches to use - untested
+ /* TODO 
+  ! needs more researches to use - untested
   let data: string;
   if (ctx.has(callbackQuery("data"))) {
     data = ctx.callbackQuery.data // works!;
@@ -35,11 +39,29 @@ const pairOptSaver = (ctx: SessionContext<trackSession>) => {
   ctx.telegram.sendMessage(chatId, message, backToMenu);
 }
 
-bot.hears("a", (ctx: SessionContext<trackSession>) => {
-  ctx.telegram.sendMessage(ctx.chat.id, `${ctx.session.triggerType}`);
-});
 
+const AddrAnalysis = (ctx: SessionContext<TrackSession>) => {
+  ctx.deleteMessage();
+  
+  //TODO! add as middleware
+  if(ctx.session?.commonStat) {
+
+    if(!Web3.utils.checkAddressChecksum(ctx.message["text"]))
+      ctx.reply(invalidAddress, backToMenu);
+  
+    if (ctx.session.triggerType !== "toPaired") {
+
+    } else {
+      // only for test - will change
+      ctx.reply("درخواست شما ثبت شد");
+
+      ctx.telegram.sendMessage(ctx.chat.id, menuMessage , mainMenu);
+    }
+  }
+}
+// if(ctx.session?.toAddr) {}
 export default trackCB;
 export {
-  pairOptSaver
+  pairOptSaver,
+  AddrAnalysis
 }
