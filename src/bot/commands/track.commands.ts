22ac9daab5a05245931f9a3873239f44e7fc12ed
farajioranj <farajioranj@gmail.com>
2023-05-29 03:10:02 +0430

    working on address receiver listener...

--- src/bot/commands/track.commands.ts
+++ src/bot/commands/track.commands.ts
@@ -1,8 +1,18 @@
 import { Composer } from "telegraf";
 import bot from "../bot.instance";
-import trackCB, { pairOptSaver } from "../handlers/track.handlers";
+import trackCB, { pairOptSaver, AddrAnalysis } from "../handlers/track.handlers";
+import Web3 from "web3";
+
 
 bot.action("track", trackCB);
 
 bot.action(["unpaired", "bothPaied", "fromPaired", "toPaired"], pairOptSaver);
 
+// type AddrRegExp = RegExp;
+// const evmAddrRegex: RegExp = /^(0x)?[0-9a-fA-F]{40}$/i;
+
+// function validateUserAddress(address: string): boolean {
+//     return evmAddrRegex.test(address);
+// } 
+
+bot.hears(/^0x(.+)/, AddrAnalysis);
