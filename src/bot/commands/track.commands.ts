1d8d76119a81ef514947fbd56deab410615ab748
farajioranj <farajioranj@gmail.com>
2023-05-28 06:56:41 +0430

    pair options buttons almost done

--- src/bot/commands/track.commands.ts
+++ src/bot/commands/track.commands.ts
@@ -1,4 +1,8 @@
+import { Composer } from "telegraf";
 import bot from "../bot.instance";
-import trackCB from "../handlers/track.handlers";
+import trackCB, { pairOptSaver } from "../handlers/track.handlers";
 
 bot.action("track", trackCB);
+
+bot.action(["unpaired", "bothPaied", "fromPaired", "toPaired"], pairOptSaver);
+
