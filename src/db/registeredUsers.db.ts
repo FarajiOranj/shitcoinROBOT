d012aa157aaddb55231dfd861944883fe39d5662
farajioranj <farajioranj@gmail.com>
2023-05-28 15:30:25 +0430

    db updated

--- src/db/registeredUsers.db.ts
+++ src/db/registeredUsers.db.ts
@@ -1,10 +1,12 @@
-let registeredUserIDS: Map<number, string | Array<string>> = new Map();
-
-registeredUserIDS.set(644473251, "0x0");
-registeredUserIDS.set(89192167, "0x0");
-registeredUserIDS.set(5443857391, "0x0");
-registeredUserIDS.set(390415311, "0x0");
-// registeredUserIDS.set(, "0x0");
+let registeredUserIDS: Map<number, string | Array<string>> = new Map(
+    [
+        [644473251, "0x0"], // Behrouz
+        [89192167, "0x0"], // Matin
+        [390415311, "0x0"], // Saeed
+        [5443857391, "0x0"], // Mohammadreza
+        [116185367, "0x0"] // Arash
+    ]
+);
 
 // type AddrRegExp = RegExp;
 // const evmAddrRegex: RegExp = /^(0x)?[0-9a-fA-F]{40}$/i;
