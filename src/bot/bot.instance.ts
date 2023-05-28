3eb20ac9f4468ad15724605087bda1c83b6e8ebb
farajioranj <farajioranj@gmail.com>
2023-05-28 07:29:59 +0430

    session seperated

--- src/bot/bot.instance.ts
+++ src/bot/bot.instance.ts
@@ -2,6 +2,6 @@ import { Telegraf, session } from 'telegraf';
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const bot: Telegraf = new Telegraf(process.env.TELEGRAM_BOT_TOKEN).use(session());
+const bot: Telegraf = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 export default bot;
