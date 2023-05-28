7dbeccd6835a747f113a12206ab834a769dabee7
farajioranj <farajioranj@gmail.com>
2023-05-28 06:56:08 +0430

    session and use case added to bot for test

--- src/bot/bot.instance.ts
+++ src/bot/bot.instance.ts
@@ -1,7 +1,7 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, session } from 'telegraf';
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const bot: Telegraf = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const bot: Telegraf = new Telegraf(process.env.TELEGRAM_BOT_TOKEN).use(session());
 
 export default bot;
