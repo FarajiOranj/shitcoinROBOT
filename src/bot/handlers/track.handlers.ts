cac12cc12c075ea71742547e22ccd7385edffd03
farajioranj <farajioranj@gmail.com>
2023-05-28 06:59:57 +0430

    pair options buttons almost done

--- src/bot/handlers/track.handlers.ts
+++ src/bot/handlers/track.handlers.ts
@@ -1,11 +1,38 @@
 import { Context } from "telegraf";
+// import { callbackQuery } from "telegraf/filters";
+import { SessionContext } from "telegraf/typings/session";
 import bot from "../bot.instance";
-import { trackMenu } from "../layout/layout";
-import { trackOpts } from "../../../public/static/trackUx";
+import { backToMenu, trackMenu } from "../layout/layout";
+import trackSession from "../../../public/types/trackSession";
+import { trackOpts, fromAddres, toAddress } from "../../../public/static/trackUx";
 
 const trackCB = (ctx: Context) => {
   ctx.deleteMessage();
   bot.telegram.sendMessage(ctx.chat.id, trackOpts, trackMenu);
 };
 
+const pairOptSaver = (ctx: SessionContext<trackSession>) => {
+  const chatId: number = ctx.chat.id;
+  const data: string = ctx.callbackQuery?.["data"];
+  
+  ctx.session.userId = chatId;
+  ctx.session.triggerType = data;
+
+  const message: string = data === "toPaired" ? toAddress : fromAddres;
+
+ /* 
+  needs more researches to use - untested
+  let data: string;
+  if (ctx.has(callbackQuery("data"))) {
+    data = ctx.callbackQuery.data // works!;
+  }
+  ctx.session.triggerType = data; 
+  */
+
+  ctx.telegram.sendMessage(chatId, message, backToMenu);
+}
+
 export default trackCB;
+export {
+  pairOptSaver
+}
