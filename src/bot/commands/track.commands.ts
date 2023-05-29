44bf5e5c8d1b698ca6711a741e6f7d0dd03784fc
farajioranj <farajioranj@gmail.com>
2023-05-30 02:18:03 +0430

    commeted regex removed

--- src/bot/commands/track.commands.ts
+++ src/bot/commands/track.commands.ts
@@ -2,17 +2,11 @@ import { Composer } from "telegraf";
 import bot from "../bot.instance";
 import trackCB, { pairOptSaver, AddrAnalysis } from "../handlers/track.handlers";
 import Web3 from "web3";
+import composedAddrMiddleware from "../middlewares/track.middlewares";
 
 
 bot.action("track", trackCB);
 
 bot.action(["unpaired", "bothPaied", "fromPaired", "toPaired"], pairOptSaver);
 
-// type AddrRegExp = RegExp;
-// const evmAddrRegex: RegExp = /^(0x)?[0-9a-fA-F]{40}$/i;
-
-// function validateUserAddress(address: string): boolean {
-//     return evmAddrRegex.test(address);
-// } 
-
-bot.hears(/^0x(.+)/, AddrAnalysis);
+bot.hears(/^0x(.+)/, composedAddrMiddleware, AddrAnalysis);
