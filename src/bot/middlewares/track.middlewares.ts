6d684e08124ffa68c2547c08e8896b05e879867e
farajioranj <farajioranj@gmail.com>
2023-05-30 02:19:20 +0430

    track middleware done

---
+++ src/bot/middlewares/track.middlewares.ts
@@ -0,0 +1,21 @@
+import { Telegraf, Context, MiddlewareFn } from "telegraf";
+import { SessionContext } from "telegraf/typings/session";
+import Web3 from "web3";
+import { backToMenu } from "../layout/layout";
+import { invalidAddress } from "../../../public/static/trackUx";
+
+
+const hasCommonStat = (ctx: SessionContext<any>, next: () => void) => {
+    if(ctx.session?.commonStat) next();
+    return;
+}
+
+const addressCheck = (ctx: Context, next: () => void) => {
+    Web3.utils.checkAddressChecksum(ctx.message["text"]) ?
+        next() :
+        ctx.reply(invalidAddress, backToMenu);
+}
+
+const composedAddrMiddleware = Telegraf.compose([hasCommonStat, addressCheck]);
+
+export default composedAddrMiddleware;
