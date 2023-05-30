ce4210e0999b48121203ff9565b21fc676ee4f34
farajioranj <farajioranj@gmail.com>
2023-05-31 02:39:05 +0430

    prettied & using static msg

--- src/bot/middlewares/common.middlewares.ts
+++ src/bot/middlewares/common.middlewares.ts
@@ -1,10 +1,9 @@
+import { Context } from "telegraf";
 import bot from "../bot.instance";
 import registeredUserIDS from "../../db/registeredUsers.db";
-import { Context } from "telegraf";
+import { notAuthenticated } from "../../../public/static/starterUserUx";
 
-
-bot.use((ctx: Context, next: () => Promise<void> ) =>{
-    if(!registeredUserIDS.has(ctx.chat.id))
-        return ctx.reply("شما احراز نشده اید")
-    return next();
+bot.use((ctx: Context, next: () => Promise<void>) => {
+  if (!registeredUserIDS.has(ctx.chat.id)) return ctx.reply(notAuthenticated);
+  return next();
 });
