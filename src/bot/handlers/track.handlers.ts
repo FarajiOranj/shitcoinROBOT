4a27677e7ef0b5f8775d57c74d625602dc4fd73c
farajioranj <farajioranj@gmail.com>
2023-05-30 06:09:34 +0430

    then added for keyboard tracker + bot instance rm

--- src/bot/handlers/track.handlers.ts
+++ src/bot/handlers/track.handlers.ts
@@ -1,17 +1,17 @@
 import { Context } from "telegraf";
 // import { callbackQuery } from "telegraf/filters";
 import { SessionContext } from "telegraf/typings/session";
-import bot from "../bot.instance";
 import { backToMenu, mainMenu, trackMenu } from "../layout/layout";
 import { TrackSession } from "../../../public/types/sessionTypes";
 import { trackOpts, fromAddres, toAddress, bothPairedWarn } from "../../../public/static/trackUx";
 import { menuMessage } from "../../../public/static/starterUserUx";
 import deleteAvailableMsg from "../../helper/deleteMsg";
 import { PairStat } from "../../../public/types/transaction";
+import storeKeyID from "../../helper/sessionKey.store";
 
 const trackCB = (ctx: Context) => {
   deleteAvailableMsg(ctx);
-  bot.telegram.sendMessage(ctx.chat.id, trackOpts, trackMenu);
+  ctx.telegram.sendMessage(ctx.chat.id, trackOpts, trackMenu);
 };
 
 //TODO! change "any" type later to an accurate type
@@ -39,7 +39,7 @@ const pairOptSaver = (ctx: SessionContext<any>) => {
   ctx.session.triggerType = data; 
   */
 
-  ctx.telegram.sendMessage(chatId, message, backToMenu);
+  ctx.telegram.sendMessage(chatId, message, backToMenu).then(storeKeyID(ctx));
 }
 
 //TODO! change "any" type later to an accurate type
@@ -66,7 +66,10 @@ const AddrAnalysis = (ctx: SessionContext<any>) => {
     } else {
       ctx.session.trackSession.fromAddr = givenAddress;
       if(triggerType === "fromPaired") sendAcceptionNotif = true;
-      else continuousMsg = toAddress;
+      else {
+        ctx.telegram.sendMessage(ctx.chat.id, "آدرس مبدا ثبت شد.", {reply_to_message_id: ctx.message.message_id});
+        continuousMsg = toAddress;
+      }
     }
   } else {
     ctx.session.trackSession.toAddr = givenAddress;
@@ -75,9 +78,9 @@ const AddrAnalysis = (ctx: SessionContext<any>) => {
   if (sendAcceptionNotif) {
     ctx.session.trackSession.completed = true;
     // only for test - will change
-    ctx.reply("درخواست شما ثبت شد");
-    ctx.telegram.sendMessage(ctx.chat.id, menuMessage, mainMenu);
-  } else ctx.telegram.sendMessage(ctx.chat.id, continuousMsg, backToMenu);
+    ctx.telegram.sendMessage(ctx.chat.id, "درخواست شما ثبت شد", {reply_to_message_id: ctx.message.message_id});
+    ctx.telegram.sendMessage(ctx.chat.id, menuMessage, mainMenu).then(storeKeyID(ctx));
+  } else ctx.telegram.sendMessage(ctx.chat.id, continuousMsg, backToMenu).then(storeKeyID(ctx));
 }
 
 export default trackCB;
