936cc13ee98f62118950699a24de7345bb33f3f9
farajioranj <farajioranj@gmail.com>
2023-05-30 06:39:48 +0430

    bug fixed

--- src/helper/sessionKey.store.ts
+++ src/helper/sessionKey.store.ts
@@ -2,7 +2,7 @@ import { Message } from "telegraf/typings/core/types/typegram";
 import { SessionContext } from "telegraf/typings/session";
 
 const storeKeyID = (ctx: SessionContext<any>) => (message: Message) :void => {
-    if(ctx.session?.keyId) ctx.session.keyId = {} as number;
+    if(!ctx.session?.keyId) ctx.session.keyId = {} as number;
     ctx.session.keyId = message.message_id;
 }
 
