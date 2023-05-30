d8e58f8a8e938f4b06a7afb2b559fc29daec3463
farajioranj <farajioranj@gmail.com>
2023-05-30 06:39:57 +0430

    compiled

--- dist/src/helper/sessionKey.store.js
+++ dist/src/helper/sessionKey.store.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const storeKeyID = (ctx) => (message) => {
     var _a;
-    if ((_a = ctx.session) === null || _a === void 0 ? void 0 : _a.keyId)
+    if (!((_a = ctx.session) === null || _a === void 0 ? void 0 : _a.keyId))
         ctx.session.keyId = {};
     ctx.session.keyId = message.message_id;
 };
