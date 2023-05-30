babdfe094de2150023a0b8c2679e98d05ab54223
farajioranj <farajioranj@gmail.com>
2023-05-31 03:49:44 +0430

    resWillReply added for better reply handling

--- src/bot/handlers/track.handlers.ts
+++ src/bot/handlers/track.handlers.ts
@@ -2,7 +2,15 @@ import { Context } from "telegraf";
 import { SessionContext } from "telegraf/typings/session";
 import { backToMenu, mainMenu, trackMenu } from "../layout/layout";
 import { TrackSession } from "../../../public/types/sessionTypes";
-import { trackOpts, fromAddres, toAddress, bothPairedWarn, fromSubmitted, reqSent } from "../../../public/static/trackUx";
+import {
+  trackOpts,
+  fromAddres,
+  toAddress,
+  bothPairedWarn,
+  fromSubmitted,
+  reqSent,
+  resWillReply,
+} from "../../../public/static/trackUx";
 import { menuMessage } from "../../../public/static/starterUserUx";
 import deleteAvailableMsg from "../../helper/deleteMsg";
 import { PairStat } from "../../../public/types/transaction";
@@ -10,7 +18,9 @@ import storeKeyID from "../../helper/sessionKey.store";
 
 const trackCB = (ctx: Context) => {
   deleteAvailableMsg(ctx);
-  ctx.telegram.sendMessage(ctx.chat.id, trackOpts, trackMenu).then(storeKeyID(ctx));
+  ctx.telegram
+    .sendMessage(ctx.chat.id, trackOpts, trackMenu)
+    .then(storeKeyID(ctx));
 };
 
 //TODO! change "any" type later to an accurate type
@@ -28,7 +38,7 @@ const pairOptSaver = (ctx: SessionContext<any>) => {
 
   const message: string = data === "toPaired" ? toAddress : fromAddres;
 
- /* TODO
+  /* TODO
   ! needs more researches to use - untested
   let data: string;
   if (ctx.has(callbackQuery("data"))) {
@@ -38,23 +48,24 @@ const pairOptSaver = (ctx: SessionContext<any>) => {
   */
 
   ctx.telegram.sendMessage(chatId, message, backToMenu).then(storeKeyID(ctx));
-}
+};
 
 //TODO! change "any" type later to an accurate type
 const AddrAnalysis = async (ctx: SessionContext<any>) => {
   const triggerType: PairStat = ctx.session.trackSession.triggerType;
   const givenAddress: string = ctx.message["text"];
+  const chatId: number = ctx.chat.id;
 
   deleteAvailableMsg(ctx);
 
   let sendAcceptionNotif: boolean;
   let continuousMsg: string;
 
-  if(triggerType !== "toPaired") {
-    if(ctx.session.trackSession?.fromAddr) {
-      if(
-        triggerType === "bothPaired" && 
-        (ctx.session.trackSession.fromAddr === givenAddress)
+  if (triggerType !== "toPaired") {
+    if (ctx.session.trackSession?.fromAddr) {
+      if (
+        triggerType === "bothPaired" &&
+        ctx.session.trackSession.fromAddr === givenAddress
       ) {
         continuousMsg = bothPairedWarn;
       } else {
@@ -63,9 +74,11 @@ const AddrAnalysis = async (ctx: SessionContext<any>) => {
       }
     } else {
       ctx.session.trackSession.fromAddr = givenAddress;
-      if(triggerType === "fromPaired") sendAcceptionNotif = true;
+      if (triggerType === "fromPaired") sendAcceptionNotif = true;
       else {
-        ctx.telegram.sendMessage(ctx.chat.id, fromSubmitted, {reply_to_message_id: ctx.message.message_id});
+        ctx.telegram.sendMessage(chatId, fromSubmitted, {
+          reply_to_message_id: ctx.message.message_id,
+        });
         continuousMsg = toAddress;
       }
     }
@@ -74,14 +87,32 @@ const AddrAnalysis = async (ctx: SessionContext<any>) => {
     sendAcceptionNotif = true;
   }
   if (sendAcceptionNotif) {
+    let replyMsgId: number;
+
     ctx.session.trackSession.completed = true;
-    await ctx.telegram.sendMessage(ctx.chat.id, reqSent, {reply_to_message_id: ctx.message.message_id});
-    ctx.telegram.sendMessage(ctx.chat.id, menuMessage, mainMenu).then(storeKeyID(ctx));
-  } else ctx.telegram.sendMessage(ctx.chat.id, continuousMsg, backToMenu).then(storeKeyID(ctx));
-}
+
+    await ctx.telegram.sendMessage(chatId, reqSent, {
+      reply_to_message_id: ctx.message.message_id,
+    });
+
+    await ctx.telegram
+      .sendMessage(
+        chatId,
+        resWillReply({
+          from: ctx.session.trackSession?.fromAddr,
+          to: ctx.session.trackSession?.toAddr,
+        })
+      )
+      .then((message) => (replyMsgId = message.message_id));
+
+    ctx.telegram
+      .sendMessage(chatId, menuMessage, mainMenu)
+      .then(storeKeyID(ctx));
+  } else
+    ctx.telegram
+      .sendMessage(chatId, continuousMsg, backToMenu)
+      .then(storeKeyID(ctx));
+};
 
 export default trackCB;
-export {
-  pairOptSaver,
-  AddrAnalysis
-}
+export { pairOptSaver, AddrAnalysis };
