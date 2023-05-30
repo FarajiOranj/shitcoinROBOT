bd740bd4b1fe59db0d033516adfd7f336f9026f0
farajioranj <farajioranj@gmail.com>
2023-05-30 06:10:34 +0430

    keyId session storage

---
+++ src/helper/sessionKey.store.ts
@@ -0,0 +1,9 @@
+import { Message } from "telegraf/typings/core/types/typegram";
+import { SessionContext } from "telegraf/typings/session";
+
+const storeKeyID = (ctx: SessionContext<any>) => (message: Message) :void => {
+    if(ctx.session?.keyId) ctx.session.keyId = {} as number;
+    ctx.session.keyId = message.message_id;
+}
+
+export default storeKeyID;
