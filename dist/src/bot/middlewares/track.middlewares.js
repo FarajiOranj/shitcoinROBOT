763281b156bf118dea7206ed6f59e6b75752de6a
farajioranj <farajioranj@gmail.com>
2023-05-31 22:50:40 +0430

    compiled

--- dist/src/bot/middlewares/track.middlewares.js
+++ dist/src/bot/middlewares/track.middlewares.js
@@ -7,6 +7,8 @@ const telegraf_1 = require("telegraf");
 const web3_1 = __importDefault(require("web3"));
 const layout_1 = require("../layout/layout");
 const trackUx_1 = require("../../../public/static/trackUx");
+const sessionKey_store_1 = __importDefault(require("../../helper/sessionKey.store"));
+const deleteMsg_1 = __importDefault(require("../../helper/deleteMsg"));
 const hasCommonStat = (ctx, next) => {
     var _a, _b;
     if ((_b = (_a = ctx.session) === null || _a === void 0 ? void 0 : _a.trackSession) === null || _b === void 0 ? void 0 : _b.commonStat)
@@ -19,9 +21,12 @@ const isCompleted = (ctx, next) => {
     return;
 };
 const addressCheck = (ctx, next) => {
-    web3_1.default.utils.checkAddressChecksum(ctx.message["text"])
-        ? next()
-        : ctx.reply(trackUx_1.invalidAddress, layout_1.backToMenu);
+    if (!web3_1.default.utils.checkAddressChecksum(ctx.message["text"])) {
+        (0, deleteMsg_1.default)(ctx);
+        ctx.reply(trackUx_1.invalidAddress, layout_1.backToMenu).then((0, sessionKey_store_1.default)(ctx));
+    }
+    else
+        next();
 };
 const composedAddrMiddleware = telegraf_1.Telegraf.compose([
     hasCommonStat,
