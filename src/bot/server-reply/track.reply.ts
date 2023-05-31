49781791ed3c65b3cb2e64715aec7d826bed8344
farajioranj <farajioranj@gmail.com>
2023-05-31 18:43:18 +0430

    server reply for single notif declared

---
+++ src/bot/server-reply/track.reply.ts
@@ -0,0 +1,10 @@
+import bot from "../bot.instance";
+import ITxData from "../../../public/types/transaction";
+
+const pendTxResToUser = (txData: ITxData, chatId: number, replyMsgId: number) => {
+    bot.telegram.sendMessage(chatId, `${txData}`,{reply_to_message_id: replyMsgId});
+}
+
+export {
+    pendTxResToUser
+}
