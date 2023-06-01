698a10e07802421bd0810506cbbdb6a72a5c1250
farajioranj <farajioranj@gmail.com>
2023-06-02 01:13:28 +0430

    testing... + logs removed

--- tests/provider/routeTracker.test.ts
+++ tests/provider/routeTracker.test.ts
@@ -1,27 +1,27 @@
+const fs = require("fs");
 import ITxData from "../../public/types/transaction";
 import pendingTxTracker from "../../src/provider/pendingTracker";
-const fs = require("fs");
+import whaleAddresses from "../../public/static/trackedAddresses";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 async function createFileCB(TxData: ITxData, comment: string) {
-  console.log("Pasting the text...");
   await fs.writeFile(
     "txHistory.js",
     `${comment}\n${JSON.stringify(TxData)}\n`,
     { flag: "a", encoding: "utf-8" },
     (err) => console.log(err)
   );
-  console.log("End of file paste.");
 }
 
 (function fileCreator() {
-  console.log("Sending request...");
   pendingTxTracker({
-    // from: whaleAddresses[0],
-    to: process.env.UNI_ROUTE2,
-    // isPaired: 'BothPaired',
+    from: whaleAddresses[0],
+    // to: process.env.UNI_ROUTE2,
+    // isPaired: 'bothPaired',
     callback: (txData: ITxData) => createFileCB(txData, "// new transaction"),
   });
-  console.log("Processing on server background");
 })();
+
 
 // asserstions, mocha and chai will be added ASAP.
