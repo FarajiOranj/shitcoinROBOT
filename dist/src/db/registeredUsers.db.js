377ce1db0c10e4daa84897ba3155b1d12dcdd229
farajioranj <farajioranj@gmail.com>
2023-06-03 01:11:41 +0430

    compiled

--- dist/src/db/registeredUsers.db.js
+++ dist/src/db/registeredUsers.db.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const registeredUserIDS = new Map([
-    [644473251, "0x0"],
-    [89192167, "0x0"],
-    [390415311, "0x0"],
+    // [644473251, "0x0"], // Behrouz
+    // [89192167, "0x0"], // Matin
+    // [390415311, "0x0"], // Saeed
     [5443857391, "0x0"],
     [6188799086, "0x0"], // Mohammadreza 2
     // [116185367, "0x0"], // Arash
--- src/db/registeredUsers.db.ts
+++ src/db/registeredUsers.db.ts
@@ -1,7 +1,7 @@
 const registeredUserIDS: Map<number, string | Array<string>> = new Map([
-  [644473251, "0x0"], // Behrouz
-  [89192167, "0x0"], // Matin
-  [390415311, "0x0"], // Saeed
+  // [644473251, "0x0"], // Behrouz
+  // [89192167, "0x0"], // Matin
+  // [390415311, "0x0"], // Saeed
   [5443857391, "0x0"], // Mohammadreza
   [6188799086, "0x0"], // Mohammadreza 2
   // [116185367, "0x0"], // Arash
