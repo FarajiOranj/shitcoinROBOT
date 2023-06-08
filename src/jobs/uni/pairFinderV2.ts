b3389a4e15431d44f816533618ba0e74c6d5b8d7
farajioranj <farajioranj@gmail.com>
2023-06-08 17:30:17 +0430

    pairFinderV2 created and coded

---
+++ src/jobs/uni/pairFinderV2.ts
@@ -0,0 +1,15 @@
+import minedTxTracker from "../../provider/minedTracker";
+import ITxData from "../../../public/types/transaction";
+import { uniPairV2 } from "../../bot/server-reply/uniPairV2.reply";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const findUniV2Pairs = async (chatId: number, totalPairs: number) => {
+  minedTxTracker({
+    to: process.env.UNI_ROUTE2,
+    callback: (txData: ITxData, calledTimes:{value:number} ): boolean =>
+      uniPairV2(txData, calledTimes, chatId, totalPairs),
+  });
+};
+
+export default findUniV2Pairs;
