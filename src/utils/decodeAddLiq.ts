39d8ca22dd2dc761682a8c5156d9a92c6d9cb511
farajioranj <farajioranj@gmail.com>
2023-06-08 17:31:22 +0430

    will be used for future

---
+++ src/utils/decodeAddLiq.ts
@@ -0,0 +1,43 @@
+import { ethers } from "ethers";
+
+type AddLiqObj = {
+  token: string;
+  to: string;
+  amountTokenDesired: number;
+  amountTokenMin: number;
+  amountETHMin: number;
+  deadline: number;
+};
+
+const abiCoder = ethers.AbiCoder.defaultAbiCoder();
+
+const decodeAddLiq = (input: string): AddLiqObj => {
+  const decodedInput = abiCoder.decode(
+    [
+      "address token",
+      "uint256 amountTokenDesired",
+      "uint256 amountTokenMin",
+      "uint256 amountETHMin",
+      "address to",
+      "uint256 deadline",
+    ],
+    `0x${input.slice(10)}`
+  );
+
+  return {
+    token: decodedInput.token,
+    to: decodedInput.to,
+    amountTokenDesired: Number(decodedInput.amountTokenDesired) / 10 ** 18,
+    amountTokenMin: Number(decodedInput.amountTokenMin) / 10 ** 18,
+    amountETHMin: Number(decodedInput.amountETHMin) / 10 ** 18,
+    deadline: Number(decodedInput.deadline),
+  };
+};
+
+// console.log(
+//   decodeAddLiq(
+//     "0xf305d71900000000000000000000000082442936178e86ab8b0c1341df34eb35c0c8a50c00000000000000000000000000000000000000064f964e68233a76f52000000000000000000000000000000000000000000000064f964e68233a76f5200000000000000000000000000000000000000000000000000000001bc16d674ec80000000000000000000000000000cc8376041353d4e56a877f25cd9053eab8ce4abc00000000000000000000000000000000000000000000000000000000647c6813"
+//   )
+// );
+
+export default decodeAddLiq;
