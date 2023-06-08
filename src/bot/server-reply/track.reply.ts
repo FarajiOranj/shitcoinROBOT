4b13961c29f7d1c57838bb9c4dfc9c295f562f91
farajioranj <farajioranj@gmail.com>
2023-06-08 17:29:05 +0430

    some minor features added

--- src/bot/server-reply/track.reply.ts
+++ src/bot/server-reply/track.reply.ts
@@ -1,16 +1,19 @@
 import bot from "../bot.instance";
 import ITxData from "../../../public/types/transaction";
 import { pendMsg } from "../../../public/static/trackUx";
+import { ITrackerFn } from "../../../public/types/transaction";
 
 
-const pendTxResToUser = (
+const pendTxResToUser :ITrackerFn["callback"] = (
   txData: ITxData,
+  calledTimes: {value: number},
   chatId: number,
   replyMsgId: number
-) => {
+) :boolean => {
   bot.telegram.sendMessage(chatId, pendMsg(txData), {
     reply_to_message_id: replyMsgId,
   });
+  return true;
 };
 
 export { pendTxResToUser };
