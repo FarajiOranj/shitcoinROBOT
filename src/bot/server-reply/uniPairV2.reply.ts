2a6b2af5cf9a781656838086a30366e7bc4f6c2b
farajioranj <farajioranj@gmail.com>
2023-06-08 17:29:19 +0430

    uniPairRv2 reply created & coded

---
+++ src/bot/server-reply/uniPairV2.reply.ts
@@ -0,0 +1,32 @@
+import bot from "../bot.instance";
+import ITxData from "../../../public/types/transaction";
+import { ITrackerFn } from "../../../public/types/transaction";
+import getPairAddress from "../../helper/pairGetter";
+// import { pendMsg } from "../../../public/static/trackUx";
+
+const uniPairV2: ITrackerFn["callback"] = (
+  txData: ITxData,
+  calledTimes: { value: number },
+  chatId: number,
+  totalPairs: number
+): boolean => {
+  const input = txData.Input.input;
+
+  if (input.includes("0xf305d719")) {
+    const pairAddr = getPairAddress(txData.TxInfo.hash);
+
+    if (pairAddr) {
+
+    bot.telegram.sendMessage(chatId, pairAddr);
+
+      if (totalPairs === calledTimes.value) return true;
+      else {
+        calledTimes.value++;
+        return false;
+      }
+    } else return false;
+  } else return false;
+
+};
+
+export { uniPairV2 };
