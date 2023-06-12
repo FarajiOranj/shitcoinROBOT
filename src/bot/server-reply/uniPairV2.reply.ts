919d050a22f1e00f1fde2c56ad9117e09d3dd80e
farajioranj <farajioranj@gmail.com>
2023-06-12 17:11:50 +0430

    uniPairV2 fixed

--- src/bot/server-reply/uniPairV2.reply.ts
+++ src/bot/server-reply/uniPairV2.reply.ts
@@ -7,53 +7,46 @@ import getTokenMetadata, { ITokenMetadata } from "../../utils/tokenMetadata";
 import * as dotenv from "dotenv";
 import { uniPairFound } from "../../../public/static/trackUx";
 import { uniPairURLs } from "../layout/linker";
+import { Log } from "alchemy-sdk";
 dotenv.config();
 
 const { ADDLIQETH_MID, PAIR_EID, WETH } = process.env;
 
-const uniPairV2: ITrackerFn["callback"] = (
+const uniPairV2: ITrackerFn["callback"] = async (
   txData: ITxData,
   wsData: IWsData,
   chatId: number,
   totalPairs: number
-): void => {
+): Promise<void> => {
   const input = txData.Input.input ?? "";
 
   if (input.includes(ADDLIQETH_MID)) {
     const { calledTimes } = wsData;
 
-    alchemy.core.getTransactionReceipt(txData.TxInfo.hash).then((res) => {
-      res.logs.map((log) => {
-        if (log.topics[0] === PAIR_EID) {
-          const mainToken: string = log.topics[1].includes(WETH)
-            ? `0x${log.topics[2].slice(26)}`
-            : `0x${log.topics[1].slice(26)}`;
-
-          let tokenMetadata: ITokenMetadata;
-          getTokenMetadata(mainToken).then((res) => (tokenMetadata = res));
-
-          const uniPair: string = `0x${log.data.slice(26, 66)}`;
-
-          bot.telegram.sendMessage(
-            chatId,
-            uniPairFound(
-              tokenMetadata.name,
-              tokenMetadata.symbol,
-              mainToken,
-              uniPair,
-              0,
-              0,
-              calledTimes.value
-            ),
-            uniPairURLs(mainToken).keyboardLayout
-          );
-
-          if (calledTimes.value >= totalPairs) {
-            alchemy.ws.off(wsData.event);
-          } else calledTimes.value++;
-        }
-      });
+    let log: Log;
+    await alchemy.core.getTransactionReceipt(txData.TxInfo.hash).then((res) => {
+      log = res.logs.find((log) => log.topics[0] === PAIR_EID);
     });
+
+    if (log) {
+      const mainToken: string = log.topics[1].includes(WETH)
+        ? `0x${log.topics[2].slice(26)}`
+        : `0x${log.topics[1].slice(26)}`;
+
+      const uniPair: string = `0x${log.data.slice(26, 66)}`;
+
+      const { name, symbol, decimals } = await getTokenMetadata(mainToken);
+
+      bot.telegram.sendMessage(
+        chatId,
+        uniPairFound(name, symbol, mainToken, uniPair, 0, 0, calledTimes.value),
+        uniPairURLs(mainToken).keyboardLayout
+      );
+
+      calledTimes.value >= totalPairs
+        ? await alchemy.ws.off(wsData.event)
+        : calledTimes.value++;
+    }
   }
 };
 
