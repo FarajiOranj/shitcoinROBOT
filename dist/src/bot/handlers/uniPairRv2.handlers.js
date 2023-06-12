f72886ff73d27d2970a2b6d9758a03bdd6ec21c4
farajioranj <farajioranj@gmail.com>
2023-06-12 23:20:24 +0430

    compiled

--- dist/src/bot/handlers/uniPairRv2.handlers.js
+++ dist/src/bot/handlers/uniPairRv2.handlers.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -18,18 +27,18 @@ const newUniPair = (ctx) => {
         .sendMessage(ctx.chat.id, trackUx_1.uniPairNums, layout_1.backToMenu)
         .then((0, sessionKey_store_1.default)(ctx));
 };
-const givenPairNum = (ctx) => {
+const givenPairNum = (ctx) => __awaiter(void 0, void 0, void 0, function* () {
     (0, deleteMsg_1.default)(ctx);
     const chatId = ctx.chat.id;
     const totalPairs = Number(ctx.message["text"]);
-    ctx.telegram
+    yield ctx.telegram
         .sendMessage(chatId, trackUx_1.reqSent, {
         reply_to_message_id: ctx.message.message_id,
     });
-    ctx.telegram
+    yield ctx.telegram
         .sendMessage(chatId, (0, trackUx_1.willSentPairs)(totalPairs));
     (0, pairFinderV2_1.default)(chatId, totalPairs);
     ctx.telegram.sendMessage(chatId, starterUserUx_1.menuMessage, layout_1.mainMenu).then((0, sessionKey_store_1.default)(ctx));
-};
+});
 exports.givenPairNum = givenPairNum;
 exports.default = newUniPair;
