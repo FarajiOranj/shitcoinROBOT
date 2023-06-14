93a2759a590b880642e19103d3ea3eb37c8f43c0
farajioranj <farajioranj@gmail.com>
2023-06-15 03:16:41 +0430

    more secure and optimized

--- src/bot/middlewares/uniPairRv2.middlewares.ts
+++ src/bot/middlewares/uniPairRv2.middlewares.ts
@@ -1,8 +1,15 @@
+import { Telegraf } from "telegraf";
 import { SessionContext } from "telegraf/typings/session";
+import isCompleted from "../../helper/compeletion.checker";
 
 const hasUniPairStat = (ctx: SessionContext<any>, next: () => void) => {
-  if (ctx.session?.commonStat === "uniPair") next();
+  if (ctx.session.trackSession?.commonStat === "uniPair") next();
   return;
 };
 
-export { hasUniPairStat };
+const composedUniMiddleware = Telegraf.compose([
+  hasUniPairStat,
+  isCompleted
+]);
+
+export default composedUniMiddleware;
