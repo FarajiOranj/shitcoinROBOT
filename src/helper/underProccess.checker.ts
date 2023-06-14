0040b7f5be9f372aad713934770c594fd544b408
farajioranj <farajioranj@gmail.com>
2023-06-15 03:16:07 +0430

    codded fulify

---
+++ src/helper/underProccess.checker.ts
@@ -0,0 +1,13 @@
+import { SessionContext } from "telegraf/typings/session";
+import deleteAvailableMsg from "./deleteMsg";
+import storeKeyID from "./sessionKey.store";
+import { backToMenu } from "../bot/layout/layout";
+
+const isUnderProccess = (ctx: SessionContext<any>, next: () => void) => {
+  if (ctx.session.underProcesses[`${ctx.callbackQuery["data"]}`]) {
+    deleteAvailableMsg(ctx);
+    ctx.reply("⛔️ تا دریافت کامل نتیجه درخواست داده شده پیشین، این گزینه غیرقابل استفاده میباشد.", backToMenu).then(storeKeyID(ctx));
+  } else next();
+};
+
+export default isUnderProccess;
