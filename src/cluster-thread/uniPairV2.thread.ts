08ba6377d328e80f4a01d169a19326acc514035e
farajioranj <farajioranj@gmail.com>
2023-06-17 07:18:59 +0430

    clustering the ws

---
+++ src/cluster-thread/uniPairV2.thread.ts
@@ -0,0 +1,15 @@
+import minedTxTracker from "../provider/minedTracker";
+import ITxData, { IWsData } from "../../public/types/transaction";
+import uniPairV2 from "../bot/server-reply/uniPairV2.reply";
+
+minedTxTracker({
+  to: process.env.UNI_ROUTE2,
+  callback: (txData: ITxData, wsData: IWsData) =>
+    uniPairV2(
+      txData,
+      wsData,
+      JSON.parse(process.argv[3]),
+      Number(process.argv[4]),
+      Number(process.argv[5])
+    ),
+});
