4cb9e449de829760adc870af4bd049e489a479cd
farajioranj <farajioranj@gmail.com>
2023-06-19 18:34:26 +0430

    uniPairV2 thread fixed

---
+++ src/child-process/uniPairV2.thread.ts
@@ -0,0 +1,14 @@
+import minedTxTracker from "../provider/minedTracker";
+import ITxData, { IWsData } from "../../public/types/transaction";
+import uniPairV2 from "../bot/server-reply/uniPairV2.reply";
+
+minedTxTracker({
+  to: process.env.UNI_ROUTE2,
+  callback: (txData: ITxData, wsData: IWsData) =>
+    uniPairV2(
+      txData,
+      wsData,
+      Number(process.argv[2]),
+      Number(process.argv[3])
+    ),
+});
