067e28eea7448c390016d3dd1ebb476a03b23604
farajioranj <farajioranj@gmail.com>
2023-06-19 18:40:47 +0430

    uniPairV2 reply fixed and coded

--- src/bot/server-reply/uniPairV2.reply.ts
+++ src/bot/server-reply/uniPairV2.reply.ts
@@ -1,5 +1,5 @@
 import bot from "../bot.instance";
-import { alchemy } from "../../provider/provider";
+import { alchemy } from "../../provider/sdk-provider";
 import getTokenMetadata from "../../utils/tokenMetadata";
 import ITxData, {
   IWsData,
@@ -9,9 +9,13 @@ import { uniPairFound } from "../../../public/static/trackUx";
 import { uniPairURLs } from "../layout/linker";
 import { Log } from "alchemy-sdk";
 import * as dotenv from "dotenv";
+import calculateTokenPrice from "../../utils/tokenMidPrice";
+import getTokenSupply from "../../utils/tokenTotalSupply";
+import calculateTokenMarketCap from "../../utils/tokenMarketCap";
+import { decodeReservedTokens } from "../../utils/reservedTokens";
 dotenv.config();
 
-const { ADDLIQETH_MID, PAIR_EID, WETH } = process.env;
+const { ADDLIQETH_MID, PAIR_EID, MINT_EID, WETH } = process.env;
 
 const uniPairV2: ITrackerFn["callback"] = async (
   txData: ITxData,
@@ -24,23 +28,57 @@ const uniPairV2: ITrackerFn["callback"] = async (
   if (input.includes(ADDLIQETH_MID)) {
     const { calledTimes } = wsData;
 
-    let log: Log;
+    let pairLog, mintLog: Log;
     await alchemy.core.getTransactionReceipt(txData.TxInfo.hash).then((res) => {
-      log = res.logs.find((log) => log.topics[0] === PAIR_EID);
+      pairLog = res.logs.find((log) => log.topics[0] === PAIR_EID);
+      if (pairLog) mintLog = res.logs.find((log) => log.topics[0] === MINT_EID);
     });
 
-    if (log) {
-      const mainToken: string = log.topics[1].includes(WETH)
-        ? `0x${log.topics[2].slice(26)}`
-        : `0x${log.topics[1].slice(26)}`;
+    if (pairLog) {
+      const token0Address: string = `0x${pairLog.topics[1].slice(26)}`;
 
-      const uniPair: string = `0x${log.data.slice(26, 66)}`;
+      const mainToken: string = pairLog.topics[1].includes(WETH)
+        ? `0x${pairLog.topics[2].slice(26)}`
+        : token0Address;
+
+      const uniPair: string = `0x${pairLog.data.slice(26, 66)}`;
 
       const { name, symbol, decimals } = await getTokenMetadata(mainToken);
 
+      const reservedTokens = decodeReservedTokens(mintLog.data);
+
+      const { priceInETH, priceInDollar, perETH, perDollar } =
+        calculateTokenPrice(
+          reservedTokens,
+          decimals,
+          mainToken === token0Address,
+          // etherprice
+        );
+
+      const totalSupply = await getTokenSupply(mainToken, decimals);
+
+      const { marketCapInDollar, marketCapInETH } = calculateTokenMarketCap(
+        totalSupply,
+        priceInDollar,
+        priceInETH
+      );
+
       await bot.telegram.sendMessage(
         chatId,
-        uniPairFound(name, symbol, mainToken, uniPair, 0, 0, calledTimes.value),
+        uniPairFound(
+          name,
+          symbol,
+          mainToken,
+          uniPair,
+          priceInDollar,
+          priceInETH,
+          marketCapInDollar,
+          marketCapInETH,
+          "As Soon As Possible...",
+          perDollar,
+          perETH,
+          calledTimes.value
+        ),
         uniPairURLs(mainToken).keyboardLayout
       );
 
