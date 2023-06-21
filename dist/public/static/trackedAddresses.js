0d49c75bfd7389ed0bc84ad9c78297eb5ccf1d83
farajioranj <farajioranj@gmail.com>
2023-06-22 02:15:13 +0430

    dist removed

--- dist/public/static/trackedAddresses.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const whaleAddresses = [
-    "0xaf2358e98683265cbd3a48509123d390ddf54534",
-];
-exports.default = whaleAddresses;
--- dist/src/helper/sessionKey.store.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const storeKeyID = (ctx) => (message) => {
-    var _a;
-    if (!((_a = ctx.session) === null || _a === void 0 ? void 0 : _a.keyId))
-        ctx.session.keyId = {};
-    ctx.session.keyId = message.message_id;
-};
-exports.default = storeKeyID;
