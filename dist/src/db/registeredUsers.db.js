b9655a41b1cffc9f6806d20d304a8a19a038a3f9
farajioranj <farajioranj@gmail.com>
2023-06-22 00:41:53 +0430

    arash registered manually

--- dist/src/db/registeredUsers.db.js
+++ dist/src/db/registeredUsers.db.js
@@ -5,7 +5,7 @@ const registeredUserIDS = new Map([
     // [89192167, "0x0"], // Matin
     // [390415311, "0x0"], // Saeed
     [5443857391, "0x0"],
-    [6188799086, "0x0"], // Mohammadreza 2
-    // [116185367, "0x0"], // Arash
+    [6188799086, "0x0"],
+    [116185367, "0x0"], // Arash
 ]);
 exports.default = registeredUserIDS;
