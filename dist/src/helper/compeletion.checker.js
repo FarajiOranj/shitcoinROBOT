9f9521705f82aa83cde2d55d513f3adcf515ebe1
farajioranj <farajioranj@gmail.com>
2023-06-22 23:58:17 +0330

    dist removed

--- dist/src/helper/compeletion.checker.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isCompleted = (ctx, next) => {
-    if (!ctx.session.trackSession.completed)
-        return next();
-    return;
-};
-exports.default = isCompleted;
--- dist/src/helper/deleteMsg.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const deleteAvailableMsg = (ctx) => {
-    try {
-        ctx.deleteMessage(ctx.session.keyId);
-    }
-    catch (_a) { }
-};
-exports.default = deleteAvailableMsg;
