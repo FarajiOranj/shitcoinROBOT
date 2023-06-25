663572c09ea2bf86e30aa74ba13f26c79e02c7f6
farajioranj <farajioranj@gmail.com>
2023-06-25 22:55:09 +0330

    more urls added to uniPairURLs

--- src/bot/layout/linker.ts
+++ src/bot/layout/linker.ts
@@ -1,17 +1,31 @@
 import Skeleton from "./skeleton";
 
-const uniPairURLs = (tokenAddress: string): Skeleton => {
+const uniPairURLs = (tokenAddress: string, pairAddress: string): Skeleton => {
   return new Skeleton([
     [
       {
-        text: "🦄 UNISWAP",
+        text: "🦄UNISWAP",
         url: `https://app.uniswap.org/#/swap?outputCurrency=${tokenAddress}`,
-    },
-      { text: "🔎 ETHscan", url: `https://etherscan.io/token/${tokenAddress}` },
+      },
+      { text: "🔎ETHscan", url: `https://etherscan.io/token/${tokenAddress}` },
+    ],
+    [
+      {
+        text: "👁‍🗨Sniffer",
+        url: `https://tokensniffer.com/token/eth/${tokenAddress}`,
+      },
+    ],
+    [
+      {
+        text: "📊Screener",
+        url: `https://dexscreener.com/ethereum/${tokenAddress}`,
+      },
+      {
+        text: "📈DEXT",
+        url: `https://www.dextools.io/app/ether/pair-explorer/${pairAddress}`,
+      },
     ],
   ]);
 };
 
-export {
-    uniPairURLs
-}
+export { uniPairURLs };
