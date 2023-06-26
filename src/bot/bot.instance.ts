5e7ca2af5bdef667ff74534baadcbac971a1f814
farajioranj <farajioranj@gmail.com>
2023-06-26 21:39:03 +0330

    activationg logs...

--- src/bot/bot.instance.ts
+++ src/bot/bot.instance.ts
@@ -1,27 +1,29 @@
 import { Telegraf, Context } from "telegraf";
-import { fork } from 'child_process';
+import { fork } from "child_process";
 const ms = require("millisecond");
 import * as dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 
-
 declare module "telegraf" {
-    interface Context {
-        ethPrice: number;
-    }
+  interface Context {
+    ethPrice: number;
+  }
 }
 
 const bot: Telegraf = new Telegraf<Context>(process.env.TELEGRAM_BOT_TOKEN, {
-    handlerTimeout: ms("172000s"),
+  handlerTimeout: ms("172000s"),
 });
 
-const ETH_PriceFork = fork("../../dist/src/child-process/forked/ethPrice.ws.js");
+const ETH_PriceFork = fork(
+  path.dirname("dist/src/child-process/forked/ethPrice.ws.js")
+);
 
 ETH_PriceFork.send("");
 
 ETH_PriceFork.on("message", (price: string) => {
-    console.log("bot.context.ethPrice is: ", bot.context.ethPrice)
+  console.log("bot.context.ethPrice is: ", bot.context.ethPrice);
 
-    bot.context.ethPrice = Number(price);
+  bot.context.ethPrice = Number(price);
 });
 export default bot;
