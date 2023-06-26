b7b65c275dbc78a70eed27d029eac698bc9ad934
farajioranj <farajioranj@gmail.com>
2023-06-27 03:02:19 +0330

    returned to Telegraf Context

--- src/bot/bot.instance.ts
+++ src/bot/bot.instance.ts
@@ -3,16 +3,8 @@ const ms = require("millisecond");
 import * as dotenv from "dotenv";
 dotenv.config();
 
-declare module "telegraf" {
-  interface Context {
-    ethPrice: { value: number };
-  }
-}
-
-const bot = new Telegraf<Context>(process.env.TELEGRAM_BOT_TOKEN, {
+const bot : Telegraf= new Telegraf<Context>(process.env.TELEGRAM_BOT_TOKEN, {
   handlerTimeout: ms("172000s"),
 });
-
-bot.context.ethPrice = {value : 0};
 
 export default bot;
