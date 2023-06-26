4acbb91a13585d5e1466ce8cfd284434427e89bc
farajioranj <farajioranj@gmail.com>
2023-06-26 19:19:22 +0330

    context extended

--- src/bot/bot.instance.ts
+++ src/bot/bot.instance.ts
@@ -1,9 +1,16 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Context } from "telegraf";
+// import { Context } from 'telegraf/typings';
 const ms = require("millisecond");
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const bot: Telegraf = new Telegraf(process.env.TELEGRAM_BOT_TOKEN, {
+declare module 'telegraf' {
+    interface Context {
+        ethPrice: number;
+    }
+  }
+
+const bot: Telegraf = new Telegraf<Context>(process.env.TELEGRAM_BOT_TOKEN, {
     handlerTimeout: ms('172000s'),
 });
 
