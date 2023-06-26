be3c8a3d062e76120ea5feb367f4bdead24842b5
farajioranj <farajioranj@gmail.com>
2023-06-27 02:16:59 +0330

    fixing back button cache about commonStat

--- src/helper/deleteMsg.ts
+++ src/helper/deleteMsg.ts
@@ -4,6 +4,10 @@ const deleteAvailableMsg = (ctx: SessionContext<any>) => {
   try {
     ctx.deleteMessage(ctx.session.keyId);
   } catch {}
+
+  if(ctx.session.trackSession?.commonStat) {
+    delete ctx.session.trackSession.commonStat;
+  }
 };
 
 export default deleteAvailableMsg;
