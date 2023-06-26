b9e1e7709a52567c16037a1fec21cbac7483e078
farajioranj <farajioranj@gmail.com>
2023-06-27 03:10:05 +0330

    rm

--- learn/test.js
+++
@@ -1,9 +0,0 @@
-import { fork } from "child_process";
-
-const child = fork("forkedPrice.js");
-
-child.send("");
-
-child.on("message", (price) => {
-  console.log("The Current ETH/USD is: ", price);
-});
