8d2b5ae1c6a5ef54c16c0213639a4b8c77578567
farajioranj <farajioranj@gmail.com>
2023-06-26 19:18:17 +0330

    mv to spawned

--- src/child-process/spawned/uniPairV2.thread.ts
+++ src/child-process/spawned/uniPairV2.thread.ts
@@ -1,6 +1,6 @@
-import minedTxTracker from "../provider/minedTracker";
-import ITxData, { IWsData } from "../../public/types/transaction";
-import uniPairV2 from "../bot/server-reply/uniPairV2.reply";
+import minedTxTracker from "../../provider/minedTracker";
+import ITxData, { IWsData } from "../../../public/types/transaction";
+import uniPairV2 from "../../bot/server-reply/uniPairV2.reply";
 
 minedTxTracker({
   to: process.env.UNI_ROUTE2,
