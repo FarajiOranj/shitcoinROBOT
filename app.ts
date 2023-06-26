684a9d49319ab7cdf9f911179c2e6f071d637ee1
farajioranj <farajioranj@gmail.com>
2023-06-26 21:28:08 +0330

    some updates due to real time ethPrice + supply

--- app.ts
+++ app.ts
@@ -1,5 +1,4 @@
 
-import { fork } from 'child_process';
 import bot from './src/bot/bot.instance';
 import './src/bot/session/default.session'
 import './src/bot/middlewares/common.middlewares';
@@ -7,14 +6,5 @@ import './src/bot/commands/common.commands';
 import './src/bot/commands/track.commands';
 import './src/bot/commands/uniPairRv2.commands';
 
-const ETH_PriceFork = fork("dist/src/child-process/forked/ethPrice.ws.js");
-
-ETH_PriceFork.send("");
-
-ETH_PriceFork.on("message", (price: string) => {
-    console.log("bot.context.ethPrice is: ", bot.context.ethPrice)
-
-    bot.context.ethPrice = Number(price);
-});
 
 bot.launch();
--- src/bot/bot.instance.ts
+++ src/bot/bot.instance.ts
@@ -1,7 +1,18 @@
 import { Telegraf, Context } from "telegraf";
+import { fork } from 'child_process';
 const ms = require("millisecond");
 import * as dotenv from "dotenv";
 dotenv.config();
+
+const ETH_PriceFork = fork("../../dist/src/child-process/forked/ethPrice.ws.js");
+
+ETH_PriceFork.send("");
+
+ETH_PriceFork.on("message", (price: string) => {
+    console.log("bot.context.ethPrice is: ", bot.context.ethPrice)
+
+    bot.context.ethPrice = Number(price);
+});
 
 declare module "telegraf" {
   interface Context {
@@ -12,7 +23,5 @@ declare module "telegraf" {
 const bot: Telegraf = new Telegraf<Context>(process.env.TELEGRAM_BOT_TOKEN, {
   handlerTimeout: ms("172000s"),
 });
-
-bot.context.ethPrice = 0;
 
 export default bot;
