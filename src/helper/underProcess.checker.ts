6e2ee924ea7308a1d79d087e26f9df14b6d162fe
farajioranj <farajioranj@gmail.com>
2023-07-01 18:18:35 +0330

    fixing some polling bugs

--- src/helper/underProcess.checker.ts
+++ src/helper/underProcess.checker.ts
@@ -3,8 +3,9 @@ import deleteAvailableMsg from "./deleteMsg";
 import storeKeyID from "./sessionKey.store";
 import { backToMenu } from "../bot/layout/layout";
 
-const isUnderProcess = (ctx: SessionContext<any>, next: () => void) => {
-  if (ctx.session?.underProcesses?.[`${ctx.callbackQuery["data"]}`]) {
+const isUnderProcess = async (ctx: SessionContext<any>, next: () => void) => {
+  const hasProcessName = await ctx.session?.underProcesses?.[`${ctx.callbackQuery["data"]}`];
+  if (hasProcessName) {
     deleteAvailableMsg(ctx);
     return ctx.reply("⛔️ تا دریافت کامل نتیجه درخواست داده شده پیشین، این گزینه غیرقابل استفاده میباشد.", backToMenu).then(storeKeyID(ctx));
   } else return next();
