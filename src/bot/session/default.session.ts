b8015c96947070dee642aa8217703380140032bf
farajioranj <farajioranj@gmail.com>
2023-07-03 03:05:00 +0330

    testing redis as session

--- src/bot/session/default.session.ts
+++ src/bot/session/default.session.ts
@@ -1,4 +1,21 @@
-import { session } from "telegraf";
+// import { session } from "telegraf";
+import RedisSession from 'telegraf-session-redis';
+// import Redis from 'ioredis';
 import bot from "../bot.instance";
 
-bot.use(session());
+
+// const redisClient = new Redis({
+//     host: 'localhost',
+//     port: 6379,
+//   });
+  
+
+  const session = new RedisSession({
+    store: {
+      host: 'localhost',
+      port: 6379,
+    },
+
+  });
+  
+  bot.use(session);
