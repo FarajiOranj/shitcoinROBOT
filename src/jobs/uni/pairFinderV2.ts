7b0c201e851fbace927385f3a0e19622d2683b83
farajioranj <farajioranj@gmail.com>
2023-07-03 20:17:29 +0330

    testing session...

--- src/jobs/uni/pairFinderV2.ts
+++ src/jobs/uni/pairFinderV2.ts
@@ -1,7 +1,7 @@
 import { Worker } from "worker_threads";
+import session from "../../bot/session/redis.session";
 import { SessionContext } from "telegraf/typings/session";
 import sharedBuffer from "../../db/worker-pool/workerSharedData.db";
-// import session from "../../bot/session/redis.session";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -18,12 +18,11 @@ const findUniV2Pairs = async (
   await pairFinderWorker.postMessage([chatId, totalPairs]);
 
   await pairFinderWorker.on("exit", () => {
-    //it gives true
-    console.log("before change: ", ctx.session.underProcesses["uniNewPair"]);
-    //it gives false
-    console.log("after change: ", ctx.session.underProcesses?.["uniNewPair"]);
+    
+    const res = session.getSession(ctx);
+    delete ctx.session.underProcesses["uniNewPair"];
+    session.saveSession(res, ctx);
   });
-  delete ctx.session.underProcesses["uniNewPair"];
 };
 
 export default findUniV2Pairs;
