6b535ac5cfaf1ec1e74d837d1c9254f22b7b1210
farajioranj <farajioranj@gmail.com>
2023-07-03 20:17:16 +0330

    log rm

--- src/helper/sessionKey.store.ts
+++ src/helper/sessionKey.store.ts
@@ -3,8 +3,6 @@ import { SessionContext } from "telegraf/typings/session";
 const storeKeyID = (ctx: SessionContext<any>, msgId: number): void => {
   if (!ctx.session?.keyId) ctx.session.keyId = {} as number;
   ctx.session.keyId = msgId;
-
-  console.log(ctx.session.keyId);
 };
 
 export default storeKeyID;
