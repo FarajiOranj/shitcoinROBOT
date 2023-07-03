fade8a62f0f9900bbffdf92c2c6b3b69aa46f8f4
farajioranj <farajioranj@gmail.com>
2023-07-03 04:38:10 +0330

    testing redis as session

--- src/helper/sessionKey.store.ts
+++ src/helper/sessionKey.store.ts
@@ -3,7 +3,7 @@ import { SessionContext } from "telegraf/typings/session";
 const storeKeyID =
   (ctx: SessionContext<any>) :void => {
     if (!ctx.session?.keyId) ctx.session.keyId = {} as number;
-    ctx.session.keyId = ctx.message.message_id;
+    ctx.session.keyId = ctx.message["message_id"];
   };
 
 export default storeKeyID;
