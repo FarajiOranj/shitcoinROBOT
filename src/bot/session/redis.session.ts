ee83ea8f45be04ea759f6489989e5bf884a67686
farajioranj <farajioranj@gmail.com>
2023-07-04 01:14:30 +0330

    using @telegraf/session

--- src/bot/session/redis.session.ts
+++ src/bot/session/redis.session.ts
@@ -1,11 +1,9 @@
-// import RedisSession from "telegraf-session-redis";
-import { session } from "telegraf";
+import { Redis } from "@telegraf/session/redis";
 
-// const session = new RedisSession({
-//   store: {
-//     host: "127.0.0.1",
-//     port: 6379,
-//   },
-// });
+const session = Redis({
+  url: "redis://127.0.0.1:6379"
+});
+
+
 
 export default session;
