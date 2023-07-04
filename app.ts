b5cb326431dc79de4339374145663ca7339b56e5
farajioranj <farajioranj@gmail.com>
2023-07-04 17:42:24 +0330

    async await rm

--- app.ts
+++ app.ts
@@ -4,37 +4,35 @@ import bot from "./src/bot/bot.instance";
 
 class Application {
   constructor(bot: Telegraf) {
-    (async () => {
-      await this.registerRedisSession();
-      await this.ethPriceWorker();
-      await this.registerBotMiddlewares();
-      await this.registerBotCommands();
-      await this.launchBot(bot);
-    })();
+    this.registerRedisSession();
+    this.ethPriceWorker();
+    this.registerBotMiddlewares();
+    this.registerBotCommands();
+    this.launchBot(bot);
   }
 
-  async registerRedisSession() {
-    await require("./src/session/redis.session");
+  registerRedisSession() {
+    require("./src/session/redis.session");
   }
 
-  async ethPriceWorker() {
-    await new Worker("./dist/src/workers/ethPrice.worker.js", {
+  ethPriceWorker() {
+    new Worker("./dist/src/workers/ethPrice.worker.js", {
       workerData: require("./src/db/worker-pool/workerSharedData.db"),
     });
   }
 
-  async registerBotMiddlewares() {
-    await require("./src/bot/middlewares/common.middlewares");
+  registerBotMiddlewares() {
+    require("./src/bot/middlewares/common.middlewares");
   }
 
-  async registerBotCommands() {
-    await require("./src/bot/commands/common.commands");
-    await require("./src/bot/commands/track.commands");
-    await require("./src/bot/commands/uniPairRv2.commands");
+  registerBotCommands() {
+    require("./src/bot/commands/common.commands");
+    require("./src/bot/commands/track.commands");
+    require("./src/bot/commands/uniPairRv2.commands");
   }
 
-  async launchBot(bot: Telegraf) {
-    await bot.launch();
+  launchBot(bot: Telegraf) {
+    bot.launch();
   }
 }
 
