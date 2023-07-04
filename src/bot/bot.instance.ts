4e60833c67a2c96bf4ece3288626897ab6b81b9e
farajioranj <farajioranj@gmail.com>
2023-07-04 16:19:36 +0330

    prettier

--- src/bot/bot.instance.ts
+++ src/bot/bot.instance.ts
@@ -3,7 +3,7 @@ const ms = require("millisecond");
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const bot : Telegraf= new Telegraf<Context>(process.env.TELEGRAM_BOT_TOKEN, {
+const bot: Telegraf = new Telegraf<Context>(process.env.TELEGRAM_BOT_TOKEN, {
   handlerTimeout: ms("172000s"),
 });
 
