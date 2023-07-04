99af446b69995fb05cc0be4622552d0ddc6282f6
farajioranj <farajioranj@gmail.com>
2023-07-04 17:28:29 +0330

    using ioredis with loggers...

--- src/session/redis.session.ts
+++ src/session/redis.session.ts
@@ -2,9 +2,9 @@ import redis from "ioredis";
 const redisClient = redis.createClient();
 
 redisClient.connect();
-redisClient.on("connect", () => console.log("Connecting to Redis..."));
+redisClient.on("connecting", () => console.log("Connecting to Redis..."));
 redisClient.on("error", (err) => console.log("Redis Error: ", err.message));
-redisClient.on("connected", () =>
+redisClient.on("connect", () =>
   console.log("Connected to Redis Successfully.")
 );
 redisClient.on("end", () => console.log("Disconnected from Redis!"));
