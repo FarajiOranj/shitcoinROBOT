218b51fbac074bb2cd22c0ade5ddc3e8188d84ac
farajioranj <farajioranj@gmail.com>
2023-07-05 17:35:22 +0330

    testing redis hset & hget

--- src/bot/handlers/common.handlers.ts
+++ src/bot/handlers/common.handlers.ts
@@ -1,37 +1,37 @@
-import crypto from "crypto";
 import { SessionContext } from "telegraf/typings/session";
 import { mainMenu } from "../layout/layout";
 import {
   starterMessage,
   menuMessage,
 } from "../../../public/static/starterUserUx";
-import storeKeyID from "../../helper/sessionKey.store";
-import deleteAvailableMsg from "../../helper/deleteMsg";
+// import storeKeyID from "../../helper/session/storeKeyId";
+// import deleteAvailableMsg from "../../helper/message/deleteMsg";
 import redisClient from "../../session/redis.session";
+import userRegistration from "../../helper/session/newUserChecker";
 
 const menuCB = async (ctx: SessionContext<any>) => {
   // await deleteAvailableMsg(ctx);
+
+  userRegistration(ctx).then((isRegistered) => {
+    if (isRegistered) console.log("user subscribed right now!");
+    else console.log("user subscribed before.");
+  });
 
   // if (!ctx?.session) {
   //   ctx.session = {} as Object;
   //   ctx.session.underProcesses = {} as Object;
   // }
 
-  const hashKey = crypto
-    .createHash("sha256")
-    .update(`${ctx.from.id}:${ctx.chat.id}`)
-    .digest("hex");
+  // const value = {newUniPair: false};
 
-  const value = {newUniPair: false};
+  // await redisClient.hset(hashKey, "underProcesses", JSON.stringify(value));
 
-  await redisClient.hset(hashKey, "underProcesses", JSON.stringify(value));
-
-  const under = await redisClient.hget(
-    hashKey,
-    "underProcesses"
-  );
-  console.log(under);
-  console.log(JSON.parse(under));
+  // const under = await redisClient.hget(
+  //   hashKey,
+  //   "underProcesses"
+  // );
+  // console.log(under);
+  // console.log(JSON.parse(under));
 
   const message: string =
     (ctx.update as any)?.message?.text === "/start"
