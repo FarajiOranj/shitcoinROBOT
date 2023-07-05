e4d8ee12ee5a715b7cf44e20962b820503c74a5d
farajioranj <farajioranj@gmail.com>
2023-07-06 03:08:38 +0330

    uniPairRv2 handler synced with ioredis

--- src/bot/handlers/uniPairRv2.handlers.ts
+++ src/bot/handlers/uniPairRv2.handlers.ts
@@ -1,7 +1,7 @@
 import { SessionContext } from "telegraf/typings/session";
 import { backToMenu } from "../layout/layout";
-import storeKeyID from "../../helper/sessionKey.store";
-import deleteAvailableMsg from "../../helper/deleteMsg";
+import storeKeyID from "../../helper/message/storeId.msg";
+import deleteAvailableMsg from "../../helper/message/delete.msg";
 import findUniV2Pairs from "../../jobs/uni/pairFinderV2";
 import {
   uniPairNums,
@@ -9,12 +9,19 @@ import {
   willSentPairs,
 } from "../../../public/static/trackUx";
 import { TrackSession } from "../../../public/types/sessionTypes";
+import { singleGetter } from "../../session/getter";
+import { singleSetter } from "../../session/setter";
+
+
+const tracker = "tracker";
+const underProcesses = "underProcesses";
 
 const newUniPair = async (ctx: SessionContext<any>) => {
   await deleteAvailableMsg(ctx);
 
-  ctx.session.trackSession = {} as TrackSession;
-  ctx.session.trackSession.commonStat = "uniPair";
+  let updatedValue =  await singleGetter(ctx, tracker);
+  updatedValue["commonStat"] = "uniPair";
+  singleSetter(ctx, tracker, updatedValue);
 
   await ctx.telegram
     .sendMessage(ctx.chat.id, uniPairNums, backToMenu)
@@ -24,8 +31,13 @@ const newUniPair = async (ctx: SessionContext<any>) => {
 const givenPairNum = async (ctx: SessionContext<any>) => {
   await deleteAvailableMsg(ctx);
 
-  ctx.session.trackSession.completed = true;
-  ctx.session.underProcesses["uniNewPair"] = true;
+  let updatedValue =  await singleGetter(ctx, tracker);
+  updatedValue["completed"] = true;
+  singleSetter(ctx, tracker, updatedValue);
+
+  let updatedProcess =  await singleGetter(ctx, underProcesses);
+  updatedProcess["uniNewPair"] = true;
+  singleSetter(ctx, underProcesses, updatedProcess);
 
   const chatId: number = ctx.chat.id;
   const totalPairs: number = Number(ctx.message["text"]);
