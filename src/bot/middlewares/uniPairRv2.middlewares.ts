0cabc3d2441caf886610bdcda87f461df3bf4d5c
farajioranj <farajioranj@gmail.com>
2023-07-06 04:39:03 +0330

    loggers added

--- src/bot/middlewares/uniPairRv2.middlewares.ts
+++ src/bot/middlewares/uniPairRv2.middlewares.ts
@@ -6,6 +6,8 @@ import { singleGetter } from "../../session/getter";
 const hasUniPairStat = async (ctx: SessionContext<any>, next: () => void) => {
   const status = await singleGetter(ctx, "tracker")["commonStat"];
 
+  console.log("In UniPair Stat Middleware status is: ", status);
+
   if (status === "uniPair") {
     console.log("Passed UniPair Stat.");
     return next();
